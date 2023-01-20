95f0a09b07fe1635eaf1a159649c6b38ce3eeea1
Jackson-Wozniak <jjw30@comcast.net>
2023-01-20 15:37:28 -0500

    inital files

---
+++ backend/src/main/java/com/api/career_mode/FlightSimCareerApplication.java
@@ -0,0 +1,13 @@
+package com.api.career_mode;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FlightSimCareerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FlightSimCareerApplication.class, args);
+	}
+
+}
---
+++ backend/src/test/java/com/api/career_mode/FlightSimCareerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.api.career_mode;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FlightSimCareerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
