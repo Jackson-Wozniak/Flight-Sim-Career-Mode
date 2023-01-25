32cf118886746a5e87759bbaba642788a93f8afe
Jackson-Wozniak <jjw30@comcast.net>
2023-01-25 16:54:46 -0500

    add sign up sheet

--- frontend/src/components/SignUpPage.jsx
+++ frontend/src/components/SignUpPage.jsx
@@ -1,6 +1,74 @@
+import '../styles/SignUpPage.css';
+import NavBar from '../components/NavBar';
+import pilotIconImg from '../images/pilot_icon.png';
+import { useState } from 'react';
+import { registerNewPilot } from '../utils/ApiCalls';
+import { useNavigate } from 'react-router-dom';
+import LoadingScreen from './LoadingScreen';
+
 function SignUpPage() {
+
+    const navigate = useNavigate();
+    const [username, setUsername] = useState("");
+    const [password, setPassword] = useState("");
+    const [pilotName, setPilotName] = useState("");
+    const [homeCountry, setHomeCountry] = useState("");
+    const [loading, setLoading] = useState(false);
+    const [errorWithRegistration, setErrorWithRegistration] = useState(false);
+
+    async function callToRegisterNewPilot(e){
+        e.preventDefault();
+        setLoading(true);
+        try{
+            let tokenRes = await registerNewPilot(username, password, pilotName, homeCountry);
+            localStorage.setItem("jwtToken", tokenRes);
+            navigate('/pilot-home');
+        }catch (ex){
+            setErrorWithRegistration(true);
+        }
+        setLoading(false);
+    }
+
+    if(loading){
+        return (
+            <LoadingScreen />
+        );
+    }
+
+    let errorPopup;
+    if(errorWithRegistration){
+        errorPopup = <button className="error-popup" onClick={() => setErrorWithRegistration(false)}>
+		                <p>There was an error in your registration attempt</p>
+                        <p className="close-error-popup">x</p>
+	                </button>
+    }
+
     return ( 
-        <div></div>
+        <div className="sign-up-page-container">
+            <NavBar fixed={false}/>
+            <div className="sign-up-page-content">
+                {errorPopup}
+                <form className="pilot-sign-up-form" onSubmit={(e) => callToRegisterNewPilot(e)}>
+                    <img src={pilotIconImg} alt="" className="pilot-sign-up-icon"></img>
+                    <h2>Begin Your Aviation Career</h2>
+                    <hr />
+                    <div className="pilot-sign-up-prompt-container">
+                        <input type="email" placeholder="Email" onChange={(e) => setUsername(e.target.value)}
+                            className="pilot-sign-up-form-prompts"/>
+                        <input type="password" placeholder="Password" onChange={(e) => setPassword(e.target.value)}
+                            className="pilot-sign-up-form-prompts"/>
+                    </div>
+                    <div className="pilot-sign-up-prompt-container">
+                        <input type="text" placeholder="Pilot First Name" onChange={(e) => setPilotName(e.target.value)} 
+                            className="pilot-sign-up-form-prompts"/>
+                        <input type="text" placeholder="Pilot Home Country" onChange={(e) => setHomeCountry(e.target.value)}
+                            className="pilot-sign-up-form-prompts"/>
+                    </div>
+
+                    <input type="submit" value="Register Pilot" className="pilot-sign-up-form-submit"/>
+                </form>
+            </div>
+        </div>
      );
 }
 
--- frontend/src/utils/ApiCalls.js
+++ frontend/src/utils/ApiCalls.js
@@ -12,20 +12,6 @@ async function loginAndObtainJwtKey(username, password){
     }
 }
 
-async function registerNewPilot(username, password){
-    let usernameQuery = "username=" + username + "&";
-    let passwordQuery= "password=" + password;
-    const response = await fetch('http://localhost:8080/api/v1/pilot/register?' + usernameQuery + passwordQuery);
-
-    if(response.status !== 200){
-        let res = await response.text();
-        throw new Error(res);
-    }else{
-        let token = await response.text();
-        return token;
-    }
-}
-
 async function findTotalPilotCount(){
     const response = await fetch('http://localhost:8080/api/v1/pilot/count');
 
@@ -38,4 +24,37 @@ async function findTotalPilotCount(){
     }
 }
 
-export {loginAndObtainJwtKey, registerNewPilot, findTotalPilotCount};
+class CreatePilotRequest{
+    username;
+    password;
+    pilotName;
+    homeCountry;
+    
+    constructor(username, password, pilotName, homeCountry){
+        this.username = username;
+        this.password= password;
+        this.pilotName = pilotName;
+        this.homeCountry = homeCountry;
+    }
+}
+
+async function registerNewPilot(username, password, pilotName, homeCountry){
+    let createPilotRequest = new CreatePilotRequest(username, password, pilotName, homeCountry);
+    const response = await fetch('http://localhost:8080/api/v1/pilot/register', {
+        method : 'POST',
+        headers : {
+            'Content-Type': 'application/json'
+        },
+        body : JSON.stringify(createPilotRequest)
+    });
+
+    if(response.status !== 200){
+        let res = await response.text();
+        throw new Error(res);
+    }else{
+        let pilotCount = await response.text();
+        return pilotCount;
+    }
+}
+
+export {loginAndObtainJwtKey, findTotalPilotCount, registerNewPilot};
