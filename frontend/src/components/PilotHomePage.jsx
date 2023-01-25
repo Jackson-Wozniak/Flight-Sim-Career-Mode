ede617cc4b84e835ca169038ef75f531dc0cfdc1
Jackson-Wozniak <jjw30@comcast.net>
2023-01-25 01:39:41 -0500

    UI for pilot homepage

--- frontend/src/components/PilotHomePage.jsx
+++ frontend/src/components/PilotHomePage.jsx
@@ -1,12 +1,15 @@
 import { useState } from "react";
 import { useEffect } from "react";
-import { useLocation } from "react-router-dom";
 import { useNavigate } from "react-router-dom";
 import NavBar from './NavBar';
+import '../styles/PilotHomePage.css';
+//credit: https://unsplash.com/photos/4UgUpo3YdKk
+import propPlaneImg from '../images/prop_plane_cockpit.jpg';
+//credit: https://unsplash.com/photos/twIzCL3YSRI
+import airlinerImg from '../images/airliner.jpg';
 
 function PilotHomepage() {
 
-    const location = useLocation();
     const navigate = useNavigate();
     const [jwtToken, setJwtToken] = useState("");
     
@@ -20,9 +23,31 @@ function PilotHomepage() {
     }, [jwtToken, navigate]);
 
     return (  
-        <div>
-            <NavBar />
-            {jwtToken};
+        <div className="pilot-home-page-container">
+            <NavBar fixed={true}/>
+            <div className="pilot-homepage-card-container">
+                <div className="pilot-homepage-info-card">
+                    <img src={propPlaneImg} alt="" className="pilot-home-page-images"/>
+                    <h1>Become a private pilot</h1>
+                    <hr />
+                    <p className="pilot-path-prompts">
+                        Climb the ranks as a private pilot, completing
+                        contracted flights carrying cargo, passengers, and more. Earn 
+                        more money as your reputation grows. Don't forget to set aside some money
+                        to upgrade your hangar!
+                    </p>
+                </div>
+                <div className="pilot-homepage-info-card">
+                    <img src={airlinerImg} alt="" className="pilot-home-page-images"/>
+                    <h1>Manage your own airline</h1>
+                    <hr />
+                    <p className="pilot-path-prompts">
+                        Take control of a full-scale airline, and act as its primary pilot.
+                        Set aside money to upgrade your fleet, so that you can grow your buisness,
+                        and fly more routes concurrently!
+                    </p>
+                </div>
+            </div>
         </div>
     );
 }
