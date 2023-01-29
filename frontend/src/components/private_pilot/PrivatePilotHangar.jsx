08d265da24937ab7ba89087214bab5ab41978cf1
Jackson-Wozniak <jjw30@comcast.net>
2023-01-29 18:23:50 -0500

    layout and structure of pilot hangar component

--- frontend/src/components/private_pilot/PrivatePilotHangar.jsx
+++ frontend/src/components/private_pilot/PrivatePilotHangar.jsx
@@ -1,11 +1,37 @@
+import '../../styles/private_pilot/PrivatePilotHangar.css';
+
 function PrivatePilotHangar(props) {
+    const numberFormatter = new Intl.NumberFormat();
+
+    function displayPilotStore(){
+
+    }
+
     return (  
-        <div>
+        <div className="private-pilot-hangar-container">
+            
+            <h1 id="hangar-container-title">Owned Planes</h1>
+            <hr id="hangar-container-seperator"/>
+
             {props.pilotHangar.map((plane, index) => {
-                return  <div key={index}>
-                            {plane.name}
+                return  <div key={index} className="private-hangar-plane-container">
+                            <div className="private-hangar-plane-info">
+                                <div className="plane-info">
+                                    <p className="plane-stats">{numberFormatter.format(plane.rangeInMiles)}mi</p>
+                                    <p className="plane-labels">range</p>
+                                </div>
+                                <p>|</p>
+                                <h1>{plane.name}</h1>
+                                <p>|</p>
+                                <div className="plane-info">
+                                    <p className="plane-stats">{plane.speedInKnots} knots</p>
+                                    <p className="plane-labels">cruising speed</p>
+                                </div>
+                            </div>
+                            <hr className="plane-seperator"/>
                         </div>
             })}
+            <button onClick={() => displayPilotStore()} className="plane-buy-button">Buy Planes</button>
         </div>
     );
 }
